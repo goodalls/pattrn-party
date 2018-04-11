07f899e33ba0baed86ad65d8946d201285716d62
Jeff Goodall <goodallhj@gmail.com>
2018-04-11 16:16:39 -0600

    updating styles on tabs

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-function openCity(evt, cityName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
+document.querySelectorAll('.tablinks').forEach((selector)=>{
+  selector.addEventListener('click', openTab);
+});
 
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName('tabcontent');
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = 'none';
-  }
-
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName('tablinks');
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(' active', '');
-  }
-
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = 'block';
-  evt.currentTarget.className += ' active';
+function openTab(event) {
+  const tabName = event.target.name;
+  const tabContent = document.getElementsByClassName('tabcontent');
+  const tablinks = document.getElementsByClassName('tablinks');
+  Object.keys(tabContent).forEach(tab => tabContent[tab].style.display = 'none');
+  Object.keys(tablinks).forEach((elem)=>{
+    tablinks[elem].className = tablinks[elem].className.replace(' active', '');
+  });
+  document.getElementById(tabName).style.display = 'block';
+  event.currentTarget.className += ' active';
 }
